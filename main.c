2e6cbd7d6005fd9b2c905d5b2d2b464f28f9e1c8
Glênio Silva Pimentel <11273108+gleniosp@users.noreply.github.com>
2025-09-02 09:20:30 +0000

    05 Declaring and defining a pointer

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 void main() {
-    /* delete line #5 and write your code in this function */
-    printf("Hello, World!\n");
+    char c = 'A';
+    char *c_ptr = &c;
+
+    printf("c=%c, &c=%p\n", c, &c);
+    printf("c_ptr=%p\n", c_ptr);
 }
