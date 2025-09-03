131733a1934a56890fedeaafe7687c9c13384d3e
Glênio Silva Pimentel <11273108+gleniosp@users.noreply.github.com>
2025-09-03 07:31:51 +0000

    07 * and & in relation to pointer variable

--- main.c
+++ main.c
@@ -4,7 +4,11 @@ void main() {
     char c = 'A';
     char *c_ptr = &c;
 
+    // check variable addresses and the address stored by the pointer
     printf("c=%c, &c=%p\n", c, &c);
     printf("c_ptr=%p\n", c_ptr);
     printf("&c_ptr=%p\n", &c_ptr);
+
+    // access value of the variable pointed by the pointer
+    printf("*c_ptr=%c\n", *c_ptr);
 }
